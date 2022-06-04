53fdf6dd5ceccc460d50a7016f8a1031dc77e98f
aselwilliams <aselwilliams22@gmail.com>
2022-06-04 10:41:39 -0400

    useContext utilized

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import MovieList from './MovieList';
 import './App.css';
-import Nav from './Nav'
+import Nav from './Nav';
+import { MovieProvider } from './MovieContext';
 
 function App() {
   return (
+    <MovieProvider>
     <div className="App">
       <Nav />
       <MovieList />
     </div>
+    </MovieProvider>
+   
   );
 }
 
--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React,{useContext} from 'react';
+import {MovieContext} from './MovieContext'
 
 function Nav() {
+    const [movies, setMovies]=useContext(MovieContext)
   return (
     <div className='nav'>
         <h3>Dev Favourites</h3>
-        <p>List of Movies: </p>
+        <p>List of Movies: {movies.length} </p>
     </div>
   )
 }
