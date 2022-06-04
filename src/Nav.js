bfc779c0416a57a53534cc71d9677c6049a81727
aselwilliams <aselwilliams22@gmail.com>
2022-06-04 10:24:00 -0400

    components are added

---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div className='nav'>
+        <h3>Dev Favourites</h3>
+        <p>List of Movies: </p>
+    </div>
+  )
+}
+
+export default Nav
